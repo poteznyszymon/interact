b54095acce47c9cd9ce55410e187100d306f435e
poteznyszymon <77408864+poteznyszymon@users.noreply.github.com>
2025-07-20 18:41:08 +0200

    init

---
+++ interact/src/test/java/com/example/interact/InteractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
