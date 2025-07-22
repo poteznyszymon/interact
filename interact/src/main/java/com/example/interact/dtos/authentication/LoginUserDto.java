515f8b3694664102b4ce78da639ba77a4fcfc76e
fularczy <szymon.fularczyk@nokia.com>
2025-07-22 11:03:58 +0200

    added unit tests for auth service

--- interact/src/main/java/com/example/interact/dtos/authentication/LoginUserDto.java
+++ interact/src/main/java/com/example/interact/dtos/authentication/LoginUserDto.java
@@ -1,8 +1,10 @@
 package com.example.interact.dtos.authentication;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class LoginUserDto {
 
     private String username;
--- interact/src/main/java/com/example/interact/dtos/authentication/RegisterUserDto.java
+++ interact/src/main/java/com/example/interact/dtos/authentication/RegisterUserDto.java
@@ -1,8 +1,10 @@
 package com.example.interact.dtos.authentication;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class RegisterUserDto {
     private String username;
     private String firstName;
